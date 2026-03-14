55b46258c73086705ba88bc8e953748ac8be1b5e
erik <eriksokolov@outlook.com>
2026-03-14 12:12:01 +0100

    start radio

---
+++ copy-launcher.ps1
@@ -0,0 +1,3 @@
+$path = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\launcher.balauncher.bat"
+cp .\launcher.bat $path
+
---
+++ launcher.bat
@@ -0,0 +1,1 @@
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process msedge.exe "http://pink.exyuserver.com/stream;""  
